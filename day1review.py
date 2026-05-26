300ff13f4e515c91263a079221438c825bfe3d83
Jhay770 <josephinwang2009@gmail.com>
2026-05-26 12:03:03 -0700

    Create day1review.py

---
+++ day1review.py
@@ -0,0 +1,7 @@
+name = "Jhay"
+age = 18
+country = "Nigeria"
+goal = "I am learning python because i want to be good at it"
+consistent = True
+
+print(f"my name is {name}, I'm {age} years old, I am from {country}. What is my goal for learning python?{goal}. Am I consistent?{consistent}")
