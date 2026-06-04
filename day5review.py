b03d6447ee2f33f08b1e0e165f6cb303fabcbf82
Jhay770 <josephinwang2009@gmail.com>
2026-06-03 18:48:20 -0700

    Create day5review.py

---
+++ day5review.py
@@ -0,0 +1,16 @@
+movie = {
+    "title": "The reality of Life",
+    "director": "Jhay",
+    "year": 2025,
+    "rating": 6.2,
+    "is_blockbuster": True
+    }
+print(movie["title"])
+print(movie["director"])
+print(movie["year"])
+print(movie["rating"])
+print(movie["is_blockbuster"])
+
+    
+print(f"A popular netflix movie called {movie["title"]}, directed by a renowned and profesional director {movie["director"]}. \nThis masterpiece was released in {movie["year"]}, and over the year it has had a rating of {movie["rating"]}. \nIs the movie rated 18+?{movie["is_blockbuster"]}")
+
