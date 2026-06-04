711f68cf9797db3a0b5c3301642d0b4b03e9e4ea
Jhay770 <josephinwang2009@gmail.com>
2026-06-03 18:44:48 -0700

    Create day4review.py

---
+++ day4review.py
@@ -0,0 +1,15 @@
+counts = [1,2,3,4,5,6,7,8,9,10]
+for count in counts:
+    if count % 2 != 0:
+        print(f"{count} is odd")
+    else:
+        print(f"{count} is even")
+
+rice = 100
+>>> beans = 200
+>>> corn = 50
+>>> if rice == 100:
+...     print("actual price")
+... elif rice < 100:
+...     print("too cheap")
+... else:
