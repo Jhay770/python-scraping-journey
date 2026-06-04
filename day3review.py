c0506433f14438ed783dbc9c810318c8925100a1
Jhay770 <josephinwang2009@gmail.com>
2026-06-03 18:37:07 -0700

    Create day3review.py

---
+++ day3review.py
@@ -0,0 +1,12 @@
+money = 100
+while money > 0:
+    print(f"I spent money, I have {money} left")
+    money = money - 10
+print("I am broke")  
+
+colours = ["pink", "blue", "green"]
+for colour in colours:
+          print(colour)
+states = ["Akwaibom", "Ondo", "Calabar"]
+for state in states:
+    print(state)
